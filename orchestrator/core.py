43f2cfcbd331f6d819e47adf8f5fe740621a0540
Mosin Shaikh <mohsin.say80@gmail.com>
2026-06-17 20:45:09 +0530

    Update core.py

--- orchestrator/core.py
+++ orchestrator/core.py
@@ -2,9 +2,9 @@ from market_intelligence.agent import MarketIntelligenceAgent
 from strategy_brain.agent import StrategyBrain
 from risk_governor.agent import RiskGovernor
 from execution_engine.agent import ExecutionEngine
-from nerve_system.bus import EventBus
 from memory_vault.memory import MemoryVault
-
+from nerve_system.bus import EventBus
+from nerve_system.topics import *
 
 class Orchestrator:
 
@@ -13,32 +13,37 @@ class Orchestrator:
         self.strategy = StrategyBrain()
         self.risk = RiskGovernor()
         self.execution = ExecutionEngine()
-        self.bus = EventBus()
+
         self.memory = MemoryVault()
+        self.bus = EventBus()
 
     def run_cycle(self):
 
-        # 1. Market Data
+        # 1. MARKET EVENT
         market_data = self.market.get_market_data()
 
+        self.bus.publish(MARKET_TOPIC, market_data)
         self.memory.save_event("MARKET", market_data)
 
-        # 2. Strategy
+        # 2. STRATEGY
         signal = self.strategy.generate_signal(market_data)
 
+        self.bus.publish(SIGNAL_TOPIC, signal)
         self.memory.save_event("SIGNAL", signal)
 
-        # 3. Risk Check
+        # 3. RISK
         decision = self.risk.evaluate(signal)
 
+        self.bus.publish(RISK_TOPIC, decision)
         self.memory.save_event("RISK", decision)
 
-        # 4. Execution
+        # 4. EXECUTION
         if decision["approved"]:
             result = self.execution.execute(signal)
         else:
             result = {"status": "rejected"}
 
+        self.bus.publish(EXECUTION_TOPIC, result)
         self.memory.save_event("EXECUTION", result)
 
         return {
